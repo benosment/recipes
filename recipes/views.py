ca81135808acada802fa74434bdd9b191f3ddb9b
Ben Osment <benjamin.j.osment@gmail.com>
2014-10-05 10:17:43 -0400

    refactor home page to use a template

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('<html><title>cookbook</title></html>')
+    return render(request, 'home.html')
